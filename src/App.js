60cc5eaf25bf4fc543a086139e54d4ecbe79b076
ARSHEET <75607915+ARSHEET@users.noreply.github.com>
2021-10-28 15:54:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HuddlePage from './components/HuddlePage'
+
+function App() {
+  return (
+    <div className="App">
+      <HuddlePage />
+    </div>
+  );
+}
+
+export default App;
